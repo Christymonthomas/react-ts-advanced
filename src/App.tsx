f788fcd4dede6aa0a25cd0294a668bc60b5537eb
Christy <christymonthomas1999@gmail.com>
2024-04-11 16:59:32 +0530

    Added Polymorphic Component(Container)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // import Input from "./Components/Input.tsx";
 import Button from "./Components/Button.tsx";
+import Container from "./Components/Container.tsx";
 
 function App() {
   return (
     <main>
       {/* <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" /> */}
-      <p>
+      {/* <p>
         <Button>A button</Button>
       </p>
       <p>
         <Button href="https://google.com">A Link</Button>
       </p>
+    // </main> */}
+      <Container as={Button}>Click Me</Container>
     </main>
   );
 }
